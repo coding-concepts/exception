f820c98d2d5c4e4b4ff72ec1b3ad64734144b85c
pgiri <pgiri@lendingclub.com>
2017-11-07 09:22:55 -0800

    Added database folder

---
+++ src/database/user.sql
@@ -0,0 +1,15 @@
+-- ---------------------------------------------------------------------------
+--      USER TABLE. STORES THE DETAILS OF A USER
+-- ---------------------------------------------------------------------------
+
+CREATE TABLE USER (
+EMAIL  VARCHAR(255) NOT NULL ,
+FIRST_NAME  VARCHAR(255) NOT NULL ,
+LAST_NAME  VARCHAR(255) NOT NULL ,
+GENDER CHAR(1) NOT NULL ,
+PHONE CHAR(10) NOT NULL ,
+DOB DATE NOT NULL ,
+PASSWORD VARCHAR(255)NOT NULL ,
+SALT  VARCHAR(255) ,
+PRIMARY KEY (EMAIL)
+);
