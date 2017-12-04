ac3de3e169d6925406d508ac5b48159a54ec3666
Totaly not Ben <jazzy4827@gmail.com>
2017-12-03 18:09:57 -0800

    Updated search form

--- src/ui/Search.java
+++ src/ui/Search.java
@@ -1,17 +1,20 @@
 package ui;
 
-import exception.ValidationError;
-import exception.ValidationException;
+import data.BookData;
+import service.BookService;
+import service.LoanService;
+import service.ServiceFactory;
+import util.FrameUtility;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.File;
+import java.util.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by sniper825 on 11/26/17.
@@ -23,7 +26,7 @@ public class Search implements IScreen {
     private JLabel Title;
     private JTextField searchBar;
     private JButton searchButton;
-    private JList ResultsList;
+    private JList<Object> ResultsList;
     private JPanel MainPanel;
     private JPanel BookInfoPanel;
     private JLabel CoverArt;
@@ -34,28 +37,75 @@ public class Search implements IScreen {
     private JButton UpdateButton;
     private JButton LoansButton;
     private JButton issueButton;
+    private JProgressBar progressBar;
+    private JScrollPane sp;
+
+    BookService bookService = ServiceFactory.getBookService();
+    LoanService loanService = ServiceFactory.getLoanService();
+
+    List<BookData> books;
+    List<String> copies;
 
     public Search() {
         ResultsList.addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
-                String[] bookInfo = ResultsList.getSelectedValue().toString().split(" - ");
-                //System.out.println(ResultsList.getSelectedValue().toString().split("-")[1]);
-                try{
-                    IdLabel.setText("ID: " + bookInfo[0]);
-                    TitleLabel.setText(bookInfo[1]);
-                    AuthorLabel.setText("Author: " + bookInfo[2]);
-                    CoverArt.setIcon(getCoverArt(bookInfo[0]));
-                } catch(java.lang.ArrayIndexOutOfBoundsException ex) {
-                    // This is because of a bad value somewhere and should not happen.
-                    System.out.println("When " + ResultsList.getSelectedValue().toString() + " is split at ' - ', it should contain 3 parts");
-                }
+                BookInfoPanel.setVisible(true);
+                try {
+                    BookData book = books.get(ResultsList.getSelectedIndex());
+                    TitleLabel.setText(book.getTitle());
+                    AuthorLabel.setText(book.getAuthor());
+                    IdLabel.setText(book.getBookId().toString());
+                    CopiesLabel.setText(copies.get(ResultsList.getSelectedIndex()));
+                    CoverArt.setIcon(getCoverArt(book.getBookId()));
+                } catch(ArrayIndexOutOfBoundsException E) {return;}
+
             }
         });
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //ResultsList.setListData(search(searchBar));
+                progressBar.setValue(5);
+                progressBar.setVisible(true);
+                BookInfoPanel.setVisible(false);
+                ResultsPanel.setVisible(false);
+                //ResultsList.setListData();
+                books  =  bookService.SearchBook(searchBar.getText());
+                copies = new ArrayList<>();
+                progressBar.setValue(35);
+                List<String>  results =  new ArrayList<>();
+                progressBar.setValue(50);
+                for(int i = 0; i < books.size(); i++) {
+                    results.add(i, books.get(i).getBookId().toString() + " - " + books.get(i).getTitle() + " - " + books.get(i).getAuthor());
+                    progressBar.setValue(util.Math.map(i * 2 - 1, 0, books.size() * 2, 50, 90));
+                    copies.add(i, bookService.getNumberOfAvailableCopies(books.get(i).getBookId()) + " out of " + bookService.getNumberOfTotalCopies(books.get(i).getBookId()) + " copies available");
+                    progressBar.setValue(util.Math.map(i * 2, 0, books.size() * 2, 50, 90));
+                }
+                ResultsList.setListData(results.toArray());
+                progressBar.setValue(100);
+                progressBar.setVisible(false);
+                ResultsPanel.setVisible(true);
+
+            }
+        });
+        UpdateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                UpdateBokForm updateBook = new UpdateBokForm();
+                //todo: add a method that will update the form with the current book.
+                FrameUtility.displayNextScreen(Search.this, updateBook, "Update a Book");
+            }
+        });
+        issueButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //todo: add a method that will take you to the issue book screen.
+            }
+        });
+        LoansButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //todo: add a method that will take you to the Loans screen.
             }
         });
     }
@@ -64,7 +114,7 @@ public class Search implements IScreen {
         return MainPanel;
     }
 
-    public static Icon getCoverArt(String id) {
+    public static Icon getCoverArt(long id) {
         ImageIcon icon = new ImageIcon("covers/NotFound.png");
         if(new File("covers/" + id + ".jpg").exists()){
             icon = new ImageIcon("covers/" + id + ".jpg");
@@ -72,5 +122,9 @@ public class Search implements IScreen {
             // todo: make a service that retrieves cover art from internet
         }
         return icon;
+    }
+
+    public static void delay(int milliseconds){
+        try {TimeUnit.MILLISECONDS.sleep(milliseconds);}catch(java.lang.InterruptedException E){return;}
     }
 }
